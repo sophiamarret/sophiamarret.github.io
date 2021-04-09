8a069e7cf20b92241aefb747933e868b1d0fec56
sophiamarret <78178541+sophiamarret@users.noreply.github.com>
2021-04-09 16:00:34 -0400

    changes

--- collections/script.js
+++ collections/script.js
@@ -1,47 +1,34 @@
-// load the airtable library, call it "Airtable";
-var Airtable = require("airtable");
-// console.log(Airtable);
+let Airtable = require("airtable");
+let books = [];
 
-// use airtable library, connect to our base using API key
-var base = new Airtable({ apiKey: "keyZoSZmqvPSck3LH" }).base(
+let base = new Airtable({ apiKey: "keyZoSZmqvPSck3LH" }).base(
   "appbGC3fHUTOPA91o"
 );
 
-// get our collection base, select all the records
-// specify functions that will receive the data
 base("books")
   .select({})
   .eachPage(gotPageOfBooks, gotAllBooks);
 
-// an empty array to hold our data
-var books = [];
-
-// callback function that receives our data
 function gotPageOfBooks(records, fetchNextPage) {
   console.log("gotPageOfBooks()");
-  // add the records from this page to our array
+
   books.push(...records);
-  // request more pages
+
   fetchNextPage();
 }
 
-// call back function that is called when all pages are loaded
 function gotAllBooks(err) {
   console.log("gotAllBooks()");
-
-  // report an error, you'd want to do something better than this in production
   if (err) {
     console.log("error loading data");
     console.error(err);
     return;
   }
 
-  // call functions to log and show the books
   consoleLogBooks();
   showBooks();
 }
 
-// just loop through the books and console.log them
 function consoleLogBooks() {
   console.log("consoleLogBooks()");
   books.forEach(book => {
@@ -49,16 +36,33 @@ function consoleLogBooks() {
   });
 }
 
-// look through our airtable data, create elements
+let pageElement = document.querySelector('.book-container');
+let infoElement = document.querySelector('#info');
+let infoDetails = document.querySelector('#details');
+
+function showInfo(event) {
+  console.log("hello")
+  if(event.target.classList.contains('row1')) {
+   infoDetails.innerHTML = `
+    <h1>${event.target.dataset.book_title}</h1>
+    <p>available: ${event.target.dataset.genre}</p>
+   `;
+
+    infoElement.classList.add('show');
+  }
+}
+
+pageElement.addEventListener('click', showInfo);
+
+
 function showBooks() {
   console.log("showBooks()");
   books.forEach(book => {
-    // create container for each book
+
     var bookContainer = document.createElement("div");
     bookContainer.classList.add("book-container");
     document.querySelector(".container").append(bookContainer);
 
-    // add book titles
     var bookTitle = document.createElement("h1");
     bookTitle.classList.add("book-title");
     bookTitle.innerText = book.fields.book_title;
@@ -69,19 +73,33 @@ function showBooks() {
     nameOfGenre.innerText = book.fields.genre;
     bookContainer.append(nameOfGenre);
 
-    var bookDescription = document.createElement("p");
-    bookDescription.classList.add("book-description");
-    bookDescription.innerText = book.fields.description;
-    bookContainer.append(bookDescription);
+  })
 
-    var bookImage = document.createElement("img");
-    bookImage.classList.add("book-image");
-    bookImage.src = book.fields.book_image[0].url;
-    bookContainer.append(bookImage);
-
-  bookContainer.addEventListener("click", function() {
-      bookDescription.classList.toggle("active");
-      bookImage.classList.toggle("active");
-    });
-   });
 }
+
+  //   var bookAuthor = document.createElement("h2");
+  //   bookAuthor.classList.add("book-author");
+  //   bookAuthor.innerText = book.fields.author;
+  //   bookContainer.append(bookAuthor);
+
+  //   var bookDescription = document.createElement("p");
+  //   bookDescription.classList.add("book-description");
+  //   bookDescription.innerText = book.fields.description;
+  //   bookContainer.append(bookDescription);
+
+  //   var bookOrder = document.createElement("p");
+  //   bookOrder.classList.add("book-order");
+  //   bookOrder.innerText = book.fields.order;
+  //   bookContainer.append(bookOrder);
+
+  //   var bookImage = document.createElement("img");
+  //   bookImage.classList.add("book-image");
+  //   bookImage.src = book.fields.book_image[0].url;
+  //   bookContainer.append(bookImage);
+
+  // bookContainer.addEventListener("click", function() {
+  //     bookDescription.classList.toggle("active");
+  //     bookImage.classList.toggle("active");
+  //     bookAuthor.classList.toggle("active");
+  //   });
+  //  });
