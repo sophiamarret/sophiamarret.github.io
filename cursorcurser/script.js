c4ba1cc1653fb4a583b694b873a7c7fa5e0fd770
sophiamarret <78178541+sophiamarret@users.noreply.github.com>
2021-05-04 20:57:15 -0400

    updates

--- cursorcurser/script.js
+++ cursorcurser/script.js
@@ -16,18 +16,28 @@ else {
 		window.addEventListener('orientationchange', rotate);
 		}
 
+let tap = document.querySelector(".tap")
+
 function rotate(event) {
 	document.querySelector("#id1").style.display = "none";
 	document.querySelector("#id2").style.display = "none";
-	document.querySelector(".tap").style.display = "block";
+	tap.style.display = "block";
 }
 
 let mobileImages = ["a.jpg", "b.jpg", "c.jpg"]
 
-document.addEventListener('touchdown', flip)
+let bg = document.querySelector('.bg');
+
+let inMobile = window.matchMedia('screen and (max-width: 812px)');
+
+if (inMobile.matches) {
+	document.removeEventListener('click', curse);
+	document.addEventListener('click', flip);
 
 function flip() {
 	console.log("flip function")
-	let random = Math.floor(Math.random() * mobileImages.length);
-    document.body.innerHTML = '<img class="words" src="' + mobileImages [random] + '">';
+	bg.style.backgroundColor = "#FAFF00";
+	let randomMobile = Math.floor(Math.random() * mobileImages.length);
+    document.body.innerHTML = '<img class="flips" src="' + mobileImages [randomMobile] + '">';
+}
 }
