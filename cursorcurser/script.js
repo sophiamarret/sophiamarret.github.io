5e7e8729c86ca9bc5e463b6badfae65679050e59
sophiamarret <78178541+sophiamarret@users.noreply.github.com>
2021-05-06 04:43:44 -0400

    update

--- cursorcurser/script.js
+++ cursorcurser/script.js
@@ -39,16 +39,3 @@ function flip() {
     document.body.innerHTML = '<img class="flips" src="' + mobileImages [randomMobile] + '">';
 }
 }
-
-// let myCursor = document.querySelector("#cursor");
-
-// window.addEventListener("mousemove", function(event)
-// {
-// 	console.log(event);
-// 	console.log("X: ");
-// 	console.log(event.pageX);
-// 	console.log("Y :");
-// 	console.log(event.pageY);
-// 	myCursor.style.top = (event.pageY - 20) + "px";
-// 	myCursor.style.left = (event.pageX - 20) + "px";
-// });
